0ba0043233dd2eb54827c6745327dc024a265713
pchitta <pcthesurfer@gmail.com>
2020-12-22 00:38:33 -0800

    Add local file version of DG client, takes too long on full Zoom lecture

--- cmd/my-service/main.go
+++ cmd/my-service/main.go
@@ -45,8 +45,47 @@ func RequestDG(audio_url string, params map[string]string) string {
 	return string(body)
 }
 
+
+func RequestDGLocal(audio_filepath string, params map[string]string) string {
+	
+	data, err := ioutil.ReadFile(audio_filepath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	requestBody, err := json.Marshal(map[string]string{
+		"data":string(data),
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := http.Client{}
+	main_url := "https://brain.deepgram.com/v2/listen"
+	request, err := http.NewRequest("POST", main_url, bytes.NewBuffer(requestBody))
+	request.SetBasicAuth(params["username"], params["password"])
+	request.Header.Set("Content-Type", "audio/m4a")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("here")
+	log.Println(string(body))
+	return string(body)
+}
 func main() {
-	audio_url := os.Args[1]
+	audio_path := os.Args[1]
+	
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter username: ")
 	user, _ := reader.ReadString('\n')
@@ -56,6 +95,11 @@ func main() {
 	pw = strings.Replace(pw, "\n", "", -1)
 	fmt.Println(user, pw)
 	params := map[string]string{"username":user, "password":pw}
-	resp := RequestDG(audio_url, params)
+	var resp string
+	if strings.HasPrefix(audio_path, "http") {
+		resp = RequestDG(audio_path, params)
+	} else {
+		resp = RequestDGLocal(audio_path, params)
+	}
 	log.Println(resp)
 }
